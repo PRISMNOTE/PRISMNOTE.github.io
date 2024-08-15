0da299a27d3fb7881bb28725121f857388e4359c
daleclack <daleclack1@foxmail.com>
2024-08-15 12:44:31 +0800

    Update Members Page

--- scripts/fillMembers.js
+++ scripts/fillMembers.js
@@ -1,50 +1,48 @@
 var presentCard = "";
 var pastCard = "";
-readCSV("info.csv", function (data){
+readCSV("info.csv", function (data) {
     if (data.length < 1) {
         alert("Error! Please contact us. ");
     }
     else {
-        for(var i = 0; i < data.length ; ++i) {
-            if (data[i].bg == "@#") {
-                pastCard +=
-                `
-                <li>
-                    <div class="card">
-                        <div></div>
-                        <a href="https://space.bilibili.com/${data[i].uid}" target="_blank">
-                            <img src="${data[i].image}">
-                        </a>
-                        <h2>${data[i].name}</h2>
-                        <pre>${data[i].desc}</pre>
-                        <pre>${data[i].work}</pre>
-                    </div>
-                </li>
-                `;
-            }
-            else {
-                presentCard +=
-                `
-                <li>
-                    <div class="card">
-                        <div style="background-image: url('${data[i].bg}');"></div>
-                        <a href="https://space.bilibili.com/${data[i].uid}" target="_blank">
-                            <img src="${data[i].image}">
-                        </a>
-                        <h2>${data[i].name}</h2>
-                        <pre>${data[i].desc}</pre>
-                        <pre>${data[i].work}</pre>
-                    </div>
-                </li>
-                `;
-            }
-            
+        for (var i = 0; i < data.length; ++i) {
+            // if (data[i].bg == "@#") {
+            //     pastCard +=
+            //     `
+            //     <li>
+            //         <div class="card">
+            //             <div></div>
+            //             <a href="https://space.bilibili.com/${data[i].uid}" target="_blank">
+            //                 <img src="${data[i].image}">
+            //             </a>
+            //             <h2>${data[i].name}</h2>
+            //             <pre>${data[i].desc}</pre>
+            //             <pre>${data[i].work}</pre>
+            //         </div>
+            //     </li>
+            //     `;
+            // }
+            // else {
+            presentCard +=
+            `
+            <li>
+                <div class="card">
+                    <div></div>
+                    <a href="https://space.bilibili.com/${data[i].uid}" target="_blank">
+                        <img src="../images/${data[i].uid}.jpg">
+                    </a>
+                    <h2>${data[i].name}</h2>
+                    <pre>${data[i].desc}</pre>
+                    <pre>${data[i].jobs}</pre>
+                </div>
+            </li>
+            `;
         }
         document.getElementById("present").innerHTML = presentCard;
-        if (pastCard != "") {
-            document.getElementById("h2").innerHTML = " 已退出成员";
-            document.getElementById("past").innerHTML = pastCard;
-        }
+        // if (pastCard != "") {
+        //     document.getElementById("h2").innerHTML = " 已退出成员";
+        //     document.getElementById("past").innerHTML = pastCard;
+        // }
     }
 });
 
